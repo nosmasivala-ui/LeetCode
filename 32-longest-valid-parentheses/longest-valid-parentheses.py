969f6357808fbf7c5d888de4eecd100b5adeecb1
Nosma Sivala <nosma.sivala@gmail.com>
2026-09-09 11:46:45 +0530

    Time: 7 ms (90.29%) | Memory: 20.5 MB (40.03%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -1,36 +1,50 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        count_1 = 0   
-        count_2 = 0   
-        max_len = 0   
+        # count_1 = 0   
+        # count_2 = 0   
+        # max_len = 0   
 
-        for i in range(len(s)):
-            if s[i] == '(':
-                count_1 += 1
-            else:
-                count_2 += 1
-            if count_1 == count_2:
-                max_len = max(max_len, count_1 + count_2)
-            elif count_2 > count_1:
-                count_1 = 0
-                count_2 = 0
+        # for i in range(len(s)):
+        #     if s[i] == '(':
+        #         count_1 += 1
+        #     else:
+        #         count_2 += 1
+        #     if count_1 == count_2:
+        #         max_len = max(max_len, count_1 + count_2)
+        #     elif count_2 > count_1:
+        #         count_1 = 0
+        #         count_2 = 0
 
-        count_1 = 0
-        count_2 = 0
+        # count_1 = 0
+        # count_2 = 0
 
-        for i in range(len(s)-1, -1, -1):
-            if s[i] == '(':
-                count_1 += 1
-            else:
-                count_2 += 1
-            if count_1 == count_2:
-                max_len = max(max_len, count_1 + count_2)
-            elif count_1 > count_2:
-                count_1 = 0
-                count_2 = 0
+        # for i in range(len(s)-1, -1, -1):
+        #     if s[i] == '(':
+        #         count_1 += 1
+        #     else:
+        #         count_2 += 1
+        #     if count_1 == count_2:
+        #         max_len = max(max_len, count_1 + count_2)
+        #     elif count_1 > count_2:
+        #         count_1 = 0
+        #         count_2 = 0
+
+        # return max_len
+
+        stack = [-1]  
+        max_len = 0
+
+        for i, ch in enumerate(s):
+            if ch == "(":
+                stack.append(i)
+            else:  
+                stack.pop()
+                if not stack:
+                    stack.append(i)  
+                else:
+                    max_len = max(max_len, i - stack[-1])
 
         return max_len
-
 
 
 
