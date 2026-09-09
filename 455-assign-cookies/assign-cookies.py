2dea9ff2a6ab7f4090086580f6e36ce27cfffb0f
Nosma Sivala <nosma.sivala@gmail.com>
2026-09-09 20:46:42 +0530

    Time: 27 ms (28.37%) | Memory: 21.8 MB (94.88%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        count = 0
+
+        for i in range(len(g)):
+            for j in range(len(s)):
+                if s[j] == g[i]:
+                    count+=1
+        
+        return count
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        
+        count = 0
+        i = j = 0
+        
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:  
+                count += 1
+                i += 1
+                j += 1
+            else:
+                j += 1   
+        
+        return count
